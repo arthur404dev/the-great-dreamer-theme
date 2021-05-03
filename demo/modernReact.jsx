e85bb09567ed423e63112e7fba6a67277559f801
Arthur Andrade <arthur.diegoo@hotmail.com>
2021-05-03 19:42:47 -0300

    :sparkles: add wsl react compatibility and new demos

---
+++ demo/modernReact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Stateless from './statelessfunctionalreact'
+
+const modernReact = () => {
+    return (
+        <div>
+            <Stateless></Stateless>
+        </div>
+    )
+}
+
+export default modernReact
